7780d4a2fc95dd60265de23486710cb01f08c0dc
Tom Fogal <tfogal@sci.utah.edu>
2017-09-12 22:01:55 -0700

    start of some parsing for the truth table.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
 extern crate csv;
+use std::io;
 
 fn main() {
+	let input_bits = 7;
+	let output_bits = 4;
+	let header_lines = 2;
+	let (inp, outp) = parse(io::stdin(), header_lines, input_bits, output_bits);
+	if (inp.len()/input_bits) != (outp.len()/output_bits) {
+		println!("Different number of input/output lines?  Parse error.");
+		std::process::exit(1);
+	}
+	let two: i32 = 2;
+	if inp.len()/input_bits != two.pow(input_bits as u32) as usize {
+		println!("Incorrect number of inputs ({}) for {} bits.",
+		         inp.len()/input_bits, input_bits);
+		std::process::exit(1);
+	}
+	println!("Parsed truth table with {} input bits -> {} output bits",
+	         input_bits, output_bits);
+	println!("({} input lines.)", inp.len()/input_bits);
 }
 
 // parses a truth table in a CSV file with
@@ -13,40 +31,40 @@ fn parse<T: std::io::Read>(data: T, nheader: usize, nin: usize, nout: usize) ->
 		.has_headers(false)
 		.from_reader(data);
 	let mut iter = rdr.records();
+	let mut line: usize = 0;
 	for _ in 0..nheader { // skip header lines.
 		iter.next();
+		line = line + 1;
 	}
-	let mut inputs: Vec<bool> = Default::default();
-	let mut outputs: Vec<bool> = Default::default();
+	let mut inputs: Vec<bool> = vec![];
+	let mut outputs: Vec<bool> = vec![];
 
 	for result in iter {
 		let record = result.expect("a CSV record");
-		println!("{:?}", record);
+		line = line + 1;
 		for i in 0..nin {
-			println!("input {}: {}", i, record[i].to_string());
 			let on: bool = match record[i].parse::<i32>() {
 				Ok(b) => b != 0,
 				Err(e) => {
-					println!("WARNING: ignoring input '{}' ({})",
-					         record[i].to_string(), e);
+					println!("WARNING: ignoring input '{}' ({}) on line {}:{}",
+					         record[i].to_string(), e, line, i);
 					false
 				},
 			};
 			inputs.push(on);
 		}
-		assert!(inputs.len() % nin == 0);
+		assert_eq!(inputs.len()%nin, 0);
 
 		// we take the right*most* NOUT columns for the outputs.  Note that this is
 		// not columns nin through nin+nout: there could be "spacer" columns
 		// between the inputs and outputs.
 		let mincol = record.len() - nout;
 		for j in mincol .. record.len() {
-			println!("output {}: {}", j, record[j].to_string());
 			let on: bool = match record[j].parse::<i32>() {
 				Ok(b) => b != 0,
 				Err(e) => {
-					println!("WARNING: ignoring output '{}' ({})",
-					         record[j].to_string(), e);
+					println!("WARNING: ignoring output '{}' ({}) on line {}:{}",
+					         record[j].to_string(), e, line, j);
 					false
 				},
 			};
@@ -54,9 +72,13 @@ fn parse<T: std::io::Read>(data: T, nheader: usize, nin: usize, nout: usize) ->
 		}
 		assert!(outputs.len() % nout == 0);
 	}
-	return (vec![], vec![]);
+	assert_eq!(inputs.len()/nin, line-nheader);
+	assert_eq!(outputs.len()/nout, line-nheader);
+	println!("retvals have {}, {} lines.", inputs.len()/nin, outputs.len()/nout);
+	return (inputs, outputs);
 }
 
+#[test]
 mod test {
 	use super::*;
 
@@ -71,6 +93,8 @@ mod test {
 	#[test]
 	fn read_test() {
 		let eg = example_head();
-		parse(eg.as_bytes(), 2, 8, 4);
+		let (inp, outp) = parse(eg.as_bytes(), 2, 8, 4);
+		// should be the same number of lines:
+		assert_eq!(inp.len() % 8, outp.len() % 4);
 	}
 }
